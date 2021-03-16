c6337e63096058641447613483c0f0fdca1b8c9d
sakawat64 <sakawathossainrony@gmail.com>
2021-03-16 17:11:09 +0600

    first commit

---
+++ app/Http/Controllers/PayOrderController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Billing\BankPaymentGateway;
+use App\Billing\PaymentGatewayContract;
+use App\Orders\OrderDetails;
+use Illuminate\Http\Request;
+
+class PayOrderController extends Controller
+{
+    public function store(OrderDetails $orderDetails, PaymentGatewayContract $paymentGateway)
+    {
+       // $paymentGateway = new PaymentGateway('usd');
+        $order = $orderDetails->all();
+        dd($paymentGateway->charge(2500));
+    }
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Providers;
+
+use App\Billing\BankPaymentGateway;
+use App\Billing\CreditPaymentGateway;
+use App\Billing\PaymentGatewayContract;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(PaymentGatewayContract::class,function ($app){
+            //return new BankPaymentGateway('usd');
+            if(request()->has('credit'))
+            {
+                return new CreditPaymentGateway('usd');
+            }
+            return new BankPaymentGateway('usd');
+
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
